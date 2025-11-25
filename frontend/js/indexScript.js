d12175a60bc4aadca8c92613af0e3bcbc76836d4
chetankhadse21 <chetankhadse2020@gmail.com>
2025-11-25 13:54:24 +0530

    menubar problem fix

--- frontend/js/indexScript.js
+++ frontend/js/indexScript.js
@@ -1,8 +1,13 @@
-function openSidebar() {
-    document.getElementById("sidebar").style.transform = "translateX(0)";
-}
+const menuToggle = document.getElementById("menuToggle");
+const sideMenu = document.getElementById("sideMenu");
+const closeMenu = document.getElementById("closeMenu");
 
-function closeSidebar() {
-    document.getElementById("sidebar").style.transform = "translateX(-250px)";
-}
+if (menuToggle && sideMenu && closeMenu) {
+  menuToggle.addEventListener("click", () => {
+      sideMenu.style.transform = "translateX(0)";
+  });
 
+  closeMenu.addEventListener("click", () => {
+      sideMenu.style.transform = "translateX(-250px)";
+  });
+}
