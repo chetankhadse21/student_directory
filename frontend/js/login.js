8fa0979bca648246dffc58fce2caf1bcbc46a0c7
DnyaneshwariRode <dnyaneshwarigr91203@gmail.com>
2025-11-27 18:10:57 +0530

    new items

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -1,11 +1,35 @@
+// LOGIN / REGISTER SWITCH
 const container = document.querySelector('.container');
 const registerBtn = document.querySelector('.register-btn');
 const loginBtn = document.querySelector('.login-btn');
 
 registerBtn.addEventListener('click', () => {
     container.classList.add('active');
-})
+});
 
 loginBtn.addEventListener('click', () => {
     container.classList.remove('active');
-})
+});
+
+
+// SIDEBAR OPEN / CLOSE
+const sideMenu = document.getElementById("sideMenu");
+const closeMenuBtn = document.getElementById("closeMenu");
+
+// Open sidebar
+function openSidebar() {
+    sideMenu.classList.add("open");
+}
+
+// Close sidebar
+closeMenuBtn.addEventListener("click", () => {
+    sideMenu.classList.remove("open");
+});
+
+
+
+console.log("JS IS RUNNING");
+
+
+
+
